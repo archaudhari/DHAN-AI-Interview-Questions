25adad3e7f607b7de3466fc79dfa6ff981bb52d5
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-03-05 23:39:36 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -65,6 +65,57 @@
 // export default App;
 
 // Here's an example code snippet that adds a "Save Colors" button to the previous example:
+import React, { useState } from 'react';
+import './style.css'; // Importing the stylesheet
+
+function App() {
+  const [boxColors, setBoxColors] = useState([
+    '#ffcc00',
+    'purple',
+    'pink',
+    'red',
+    '#00bfff',
+    '#32cd32',
+    '#ffb6c1',
+    'black',
+  ]);
+
+  const shuffleColors = () => {
+    setBoxColors((prevColors) => {
+      // Create a copy of the previous colors array
+      const newColors = [...prevColors];
+
+      // Shuffle the colors using Fisher-Yates algorithm
+      for (let i = newColors.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newColors[i], newColors[j]] = [newColors[j], newColors[i]];
+      }
+
+      // Return the shuffled colors array
+      return newColors;
+    });
+  };
+
+  const saveColors = () => {
+    // TODO: Implement save functionality
+    console.log('Colors saved:', boxColors);
+  };
+
+  return (
+    <div className="container">
+      <button onClick={shuffleColors}>Shuffle Colors</button>
+      <button onClick={saveColors}>Save Colors</button>
+      {/* Looping through the boxColors state array */}
+      {boxColors.map((color, i) => (
+        <div className={`box ${color === 'black' ? 'black' : ''}`} key={i}>
+          {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
 
 
 
